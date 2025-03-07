deb3b835c61482e0941667a0a567f2117fbb324e
Jan Miksovsky <jan@miksovsky.com>
2025-03-07 11:47:55 -0800

    Comments

--- src/assets/main.js
+++ src/assets/main.js
@@ -2,9 +2,28 @@
 // This is client code loaded by main page template
 //
 
+/**
+ * Manage the side navigation and the navigation menu.
+ *
+ * At larger window sizes, we display the navigation on the side of the page,
+ * but on mobile we use a <details> element to show a menu instead.
+ *
+ * In theory those could both use the same navigation content, but one issue is
+ * that the `open` state of a details element can't be controlled through a CSS
+ * media query. We tried having code that would keep these in sync, but this
+ * resulted in flicker.
+ *
+ * Instead, we copy the contents of the side navigation to the navigation menu
+ * when the page loads. We then rely on media queries to show the correct form
+ * of navigation.
+ *
+ * Code is also required to close the menu.
+ */
+
 let sideNav;
 let navMenu;
 
+// Wait until the navigation elements are available
 window.addEventListener("load", () => {
   // Copy the contents of the side navigation to the navigation menu
   sideNav = document.getElementById("sideNav");
@@ -25,10 +44,12 @@ window.addEventListener("load", () => {
   });
 });
 
+// Hide the menu when navigation away
 window.addEventListener("pagehide", () => {
   navMenu.open = false;
 });
 
+// Clicking outside an open menu dismisses it
 document.addEventListener("click", (event) => {
   if (navMenu.open && !navMenu.contains(event.target)) {
     navMenu.open = false;
