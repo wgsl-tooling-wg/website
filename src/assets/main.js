45213e3c88bf83b30917f140c279fdd3ff4b0cac
Jan Miksovsky <jan@miksovsky.com>
2025-03-06 22:01:06 -0800

    Another shot at reducing menu flicker

--- src/assets/main.js
+++ src/assets/main.js
@@ -2,24 +2,35 @@
 // This is client code loaded by main page template
 //
 
-// Manage the display of the menu with the table of contents
+let sideNav;
+let navMenu;
 
 window.addEventListener("load", () => {
+  // Copy the contents of the side navigation to the navigation menu
+  sideNav = document.getElementById("sideNav");
+  navMenu = document.getElementById("navMenu");
+  if (sideNav && navMenu) {
+    const clone = sideNav.cloneNode(true);
+    clone.removeAttribute("id");
+    navMenu.appendChild(clone);
+  }
+
   // The pagehide handler below should be sufficient, but on iOS at least it's
   // too slow to act and can leave the menu visible when navigating back, so we
   // also try to close the menu before the navigation occurs.
-  document.getElementById("menu").addEventListener("click", (event) => {
+  navMenu.addEventListener("click", (event) => {
     if (event.target instanceof HTMLAnchorElement) {
-      menu.open = false;
+      navMenu.open = false;
     }
   });
 });
+
 window.addEventListener("pagehide", () => {
-  document.getElementById("menu").open = false;
+  navMenu.open = false;
 });
-window.addEventListener("pageshow", () => {
-  toggleTocMenu();
-});
-window.addEventListener("resize", () => {
-  toggleTocMenu();
+
+document.addEventListener("click", (event) => {
+  if (navMenu.open && !navMenu.contains(event.target)) {
+    navMenu.open = false;
+  }
 });
